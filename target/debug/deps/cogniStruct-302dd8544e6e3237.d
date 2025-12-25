ee86ecb75695472219c7769e2bf20b42c9a903f6
DnullP <dnull123p@gmail.com>
2025-12-25 22:20:35 +0800

    Init tauri

---
+++ target/debug/deps/cogniStruct-302dd8544e6e3237.d
@@ -0,0 +1,5 @@
+/Users/kaiqiu/Documents/projects/rust/cogniStruct/target/debug/deps/cogniStruct-302dd8544e6e3237.d: src/main.rs
+
+/Users/kaiqiu/Documents/projects/rust/cogniStruct/target/debug/deps/libcogniStruct-302dd8544e6e3237.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cogniStruct-6e5594ac244c8641.d
@@ -0,0 +1,5 @@
+/Users/kaiqiu/Documents/projects/rust/cogniStruct/target/debug/deps/cogniStruct-6e5594ac244c8641.d: src/main.rs
+
+/Users/kaiqiu/Documents/projects/rust/cogniStruct/target/debug/deps/libcogniStruct-6e5594ac244c8641.rmeta: src/main.rs
+
+src/main.rs:
